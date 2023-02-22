3df624ca098a8761c29bd8c7df186b918e6a8eec
satyarathi <jitendrasatyarathi@gmail.com>
2023-02-22 23:57:16 +0530

    [Jitendra]Added:Test case for user registration

--- fundoo-app/tests/integration/user.test.js
+++ fundoo-app/tests/integration/user.test.js
@@ -5,37 +5,41 @@ import mongoose from 'mongoose';
 import app from '../../src/index';
 
 describe('User APIs Test', () => {
-  before((done) => {
-    const clearCollections = () => {
-      for (const collection in mongoose.connection.collections) {
-        mongoose.connection.collections[collection].deleteOne(() => {});
-      }
-    };
+    before((done) => {
+        const clearCollections = () => {
+            for (const collection in mongoose.connection.collections) {
+                mongoose.connection.collections[collection].deleteOne(() => {});
+            }
+        };
 
-    const mongooseConnect = async () => {
-      await mongoose.connect(process.env.DATABASE_TEST);
-      clearCollections();
-    };
+        const mongooseConnect = async() => {
+            await mongoose.connect(process.env.DATABASE_TEST);
+            clearCollections();
+        };
 
-    if (mongoose.connection.readyState === 0) {
-      mongooseConnect();
-    } else {
-      clearCollections();
-    }
+        if (mongoose.connection.readyState === 0) {
+            mongooseConnect();
+        } else {
+            clearCollections();
+        }
 
-    done();
-  });
-
-  describe('GET /users', () => {
-    it('should return empty array', (done) => {
-      request(app)
-        .get('/api/v1/users')
-        .end((err, res) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.body.data).to.be.an('array');
-
-          done();
-        });
+        done();
     });
-  });
+
+    //test case for Registration
+    describe('Register /users', () => {
+        it('should return user', (done) => {
+            let userDetails = { "firstName": "Jitendra", "lastName": "Satyarathi", "email": "jitendra@gmail.com", "password": "Why5hould1te11" }
+
+            request(app)
+                .post('/api/v1/users/register')
+                .send(userDetails)
+                .end((err, res) => {
+                    expect(res.statusCode).to.be.equal(201);
+                    done();
+                });
+        });
+
+    });
+
 });
